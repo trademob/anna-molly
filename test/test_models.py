f368317949b3d376d7a297fa5fdce38b64d212a4
tobias kuhn <tobias.kuhn@trademob.com>
2015-08-14 11:06:44 +0200

    new redis model storage model
    config file update
    
    rename model

--- test/test_models.py
+++ test/test_models.py
@@ -14,6 +14,7 @@ class TestModels(unittest.TestCase):
         self.timestamp = 1234
         self.value = 66.6
         self.ttl = 60
+        self.interval = 300
         self.tst = models.TimeSeriesTuple(self.name, self.timestamp, self.value)
 
     def test_TimeSeriesTuple(self):
@@ -27,4 +28,19 @@ class TestModels(unittest.TestCase):
     def test_RedisTimestamped(self):
         rt = models.RedisTimeStamped({"ttl": self.ttl}, self.tst)
         rt.should.have.property('datapoint')
+        rt.should.have.property('name').equal('cpu:1234')
+        rt.should.have.property('ttl').equal(60)
+        rt.datapoint.should.be.a(models.TimeSeriesTuple)
+
+    def test_RedisIntervalTimeStamped(self):
+        rt = models.RedisIntervalTimeStamped({"ttl": self.ttl, "interval": self.interval}, self.tst)
+        rt.should.have.property('datapoint')
+        rt.should.have.property('name').equal('cpu:1200')
+        rt.should.have.property('ttl').equal(60)
+        rt.datapoint.should.be.a(models.TimeSeriesTuple)
+
+    def test_RedisGeneric(self):
+        rt = models.RedisGeneric('generic', self.tst)
+        rt.should.have.property('datapoint')
+        rt.should.have.property('name').equal('generic')
         rt.datapoint.should.be.a(models.TimeSeriesTuple)
